92668a2a9ff9282256e96eced920ae64f10d2aea
Kira Klingenberg <kirak@pdx.edu>
2025-05-22 18:53:00 -0700

    Added main calling routines to DFS files

--- DFS/dfs_iter_matrix.py
+++ DFS/dfs_iter_matrix.py
@@ -12,8 +12,30 @@ def dfs_iter_matrix(graph, start, total):
             order.append(current_node)
             neighbor = 0
             while neighbor < total:
-                if graph[current_node][neighbor] == 1 and visited[neighbor] == False
+                if graph[current_node][neighbor] == 1 and visited[neighbor] == False:
                     stack.append(neighbor)
                 neighbor += 1
 
+    return order
 
+
+#---MAIN CALLING ROUTINE---#
+
+matrix = [
+    [0,0,0,0,0,0,0,0,0,0,1],
+    [0,0,1,0,0,1,0,1,0,0,0],
+    [0,1,0,1,0,1,1,0,1,0,0],
+    [0,0,1,0,0,0,1,0,0,0,0],
+    [0,0,0,0,0,1,0,0,1,1,0],
+    [0,1,1,0,1,0,0,0,0,0,0],
+    [0,0,1,1,0,0,0,0,0,0,0],
+    [0,1,0,0,0,0,0,0,0,0,0],
+    [0,0,1,0,1,0,0,0,0,0,0],
+    [0,0,0,0,1,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,1,0]
+]
+
+start = 5
+total_nodes = 11
+order_visited = dfs_iter_matrix(matrix, start, total_nodes)
+print(order_visited)
--- DFS/dfs_recursive_matrix.py
+++ DFS/dfs_recursive_matrix.py
@@ -15,3 +15,24 @@ def dfs_recursive_matrix(graph, current, total, visited, order):
         if graph[current][neighbor] == 1 and visited[neighbor] == False:
             dfs_recursive_matrix(graph, neighbor, total, visited, order)
         neighbor += 1
+
+
+#---MAIN CALLING ROUTINE---#
+
+matrix = [
+    [0,0,0,0,0,0,0,0,0,0,1],
+    [0,0,1,0,0,1,0,1,0,0,0],
+    [0,1,0,1,0,1,1,0,1,0,0],
+    [0,0,1,0,0,0,1,0,0,0,0],
+    [0,0,0,0,0,1,0,0,1,1,0],
+    [0,1,1,0,1,0,0,0,0,0,0],
+    [0,0,1,1,0,0,0,0,0,0,0],
+    [0,1,0,0,0,0,0,0,0,0,0],
+    [0,0,1,0,1,0,0,0,0,0,0],
+    [0,0,0,0,1,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,1,0]
+]
+
+start = 5
+total_nodes = 11
+dfs_wrapper_matrix(matrix, start, total_nodes)
